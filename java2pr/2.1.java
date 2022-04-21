91e684a0439b12eb1f7ba2491ae79d2f0ee1ce5b
Sterlikova <91320905+Sterlikova@users.noreply.github.com>
2022-04-21 16:29:53 +0300

    Add files via upload

---
+++ java2pr/2.1.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	public static void main(String[] args) {
+        int n=1000;
+        int []acceptableArray=new int[]{1,3,5};
+        int [] array=new int[n];
+        int countAcceptable=0;
+        for (int i=0;i<array.length;i++) {
+            array[i]= (int) (Math.random()*100);
+          
+        }
+
+        for (int i=0;i<array.length;i++){
+            for(int j=0;j<acceptableArray.length;j++){
+                if (array[i]==acceptableArray[j]){
+                    countAcceptable++;
+                    break;
+                }
+            }
+        }
+        System.out.println(countAcceptable);
+
+
+
+    }
+
+}
