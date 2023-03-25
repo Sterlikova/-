602c8c91e2b922a399bc780413756d0fce315c0b
Sterlikova <91320905+Sterlikova@users.noreply.github.com>
2023-03-25 20:04:34 +0300

    Delete pr2_7.py

--- pr2_7.py
+++
@@ -1,9 +0,0 @@
-def w():
-    print("ввод числа")
-    year=int(input("год-"))
-    print("ответ")
-    if((year % 4==0 and year % 100 !=0)or year % 400==0):
-        return "да"
-    else:
-        return "нет"
-print(w())
