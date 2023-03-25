2f66d7382c917ff65ad7a531cbcb83bb098d0eff
Sterlikova <91320905+Sterlikova@users.noreply.github.com>
2023-03-25 20:04:16 +0300

    Delete pr2_4.py

--- pr2_4.py
+++
@@ -1,9 +0,0 @@
-def s():
-    print("ввод чисел")
-    a=int(input())
-    b=int(input())
-    l=int(input())
-    n=int(input())
-    print("ответ:")
-    return (2*n-1)*a+2*l+2*(n-1)*b
-print(s())
